e36a3a693e5fdb427716484c2ac4d869aa1d4b33
Victormartins2413 <132926388+Victormartins2413@users.noreply.github.com>
2024-10-14 20:38:01 -0300

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -7,19 +7,19 @@ image = Image.open("Cidade_de_Aurora.png")
 # Configurar o layout do Streamlit
 st.set_page_config(page_title="Aurora's Realm: The Enchanted Adventure", layout="centered")
 
-# Exibir a imagem de fundo
-st.image(image, use_column_width=True)
-
 # Criar uma seção de estilo para o CSS
 st.markdown(
     """
     <style>
+        .image-container {
+            position: relative;
+            text-align: center;
+        }
         .overlay {
             position: absolute;
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
-            text-align: center;
         }
         .btn {
             margin: 10px;
@@ -32,24 +32,28 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Criar uma variável de sessão para controlar a visibilidade dos botões
-if 'show_buttons' not in st.session_state:
-    st.session_state.show_buttons = False
+# Criar um contêiner para a imagem
+with st.container():
+    st.markdown('<div class="image-container">', unsafe_allow_html=True)
+    st.image(image, use_column_width=True)
 
-# Criar um contêiner para sobrepor os botões
-st.markdown('<div class="overlay">', unsafe_allow_html=True)
+    # Criar uma variável de sessão para controlar a visibilidade dos botões
+    if 'show_buttons' not in st.session_state:
+        st.session_state.show_buttons = False
 
-# Adicionar o botão "Start"
-if st.button("Start", key="start", help="Clique para começar"):
-    st.session_state.show_buttons = True
-    st.write("Você clicou em 'Start'!")
+    # Adicionar o botão "Start" em cima da imagem
+    if st.button("Start", key="start", help="Clique para começar"):
+        st.session_state.show_buttons = True
+        st.write("Você clicou em 'Start'!")
 
-# Exibir os botões "Login" e "Cadastro" apenas se "Start" for clicado
-if st.session_state.show_buttons:
-    if st.button("Login", key="login", help="Clique para fazer login"):
-        st.write("Você clicou em 'Login'!")
+    # Exibir os botões "Login" e "Cadastro" apenas se "Start" for clicado
+    if st.session_state.show_buttons:
+        st.markdown('<div class="overlay">', unsafe_allow_html=True)
+        if st.button("Login", key="login", help="Clique para fazer login"):
+            st.write("Você clicou em 'Login'!")
 
-    if st.button("Cadastro", key="cadastro", help="Clique para se cadastrar"):
-        st.write("Você clicou em 'Cadastro'!")
+        if st.button("Cadastro", key="cadastro", help="Clique para se cadastrar"):
+            st.write("Você clicou em 'Cadastro'!")
+        st.markdown('</div>', unsafe_allow_html=True)
 
-st.markdown('</div>', unsafe_allow_html=True)
+    st.markdown('</div>', unsafe_allow_html=True)
