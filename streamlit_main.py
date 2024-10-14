810ad913439ca3f0b24d3403022a8c845f819a6e
Victormartins2413 <132926388+Victormartins2413@users.noreply.github.com>
2024-10-14 20:15:52 -0300

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,43 +1,42 @@
 import streamlit as st
 from PIL import Image
 
-# Ligação dos outros arquivos:
+# Ligação dos outros arquivos
 from Login import main as login_main
 from Cadastro import main as register_main
 
-# Definição da função principal
+# Função principal
 def main():
+    # Configurações da página
     st.set_page_config(page_title="Aurora's Realm: The Enchanted Adventure", page_icon="🧙‍♀️", layout="centered")
 
-    # Carregar e exibir a imagem de fundo
+    # Carregar a imagem de fundo
     image = Image.open("Cidade_de_Aurora.png")
-    st.image(image, caption="Aurora's Realm: The Enchanted Adventure", use_column_width=True)
+    st.image(image, use_column_width=True, caption="")  # Imagem de fundo
 
     # Adicionar um espaço entre a imagem e o texto
-    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown("<br><br>", unsafe_allow_html=True)
 
-    # Mensagem de boas-vindas
-    st.markdown("## Bem-vindo ao Aurora's Realm: The Enchanted Adventure!")
+    # Mensagem de boas-vindas centralizada
+    st.markdown("<h1 style='text-align: center; color: white;'>Bem-vindo ao Aurora's Realm: The Enchanted Adventure!</h1>", unsafe_allow_html=True)
 
-    # Botão "Start"
-    if st.button("Start"):
+    # Botão "Start" centralizado
+    if st.button("Start", key="start_button"):
         offer_options()
 
 # Função para oferecer as opções de Login e Cadastro
 def offer_options():
-    st.markdown("### Escolha uma opção:")
+    st.markdown("<h2 style='text-align: center; color: white;'>Escolha uma opção:</h2>", unsafe_allow_html=True)
 
-    # Botões para Login e Cadastro
+    # Botões para Login e Cadastro em colunas
     col1, col2 = st.columns(2)
 
     with col1:
         if st.button("Login"):
-            st.write("Redirecionando para Login...")
             login_main()  # Chamar a função principal do login
 
     with col2:
         if st.button("Cadastro"):
-            st.write("Redirecionando para Cadastro...")
             register_main()  # Chamar a função principal do cadastro
 
 # Executar a função principal se este arquivo for executado
